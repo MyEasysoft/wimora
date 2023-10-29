a573d7e2c85b12efdf29524ef544329394270212
admis <admisrael2012@gmail.com>
2023-10-29 13:28:14 +0100

    Get more listings paid for

--- server/api/update-profile.js
+++ server/api/update-profile.js
@@ -23,32 +23,43 @@ module.exports = (req, res) => {
     amountPaid:req.body.resource.purchase_units[0].amount,      //Amount paid, this can be full payment or part payment
     datetimeOfPayment:req.body.resource.create_time             //The time the money was paid
   };
-  
-  
-    integrationSdk.users.updateProfile({
-        id: ownerId,
-        
-       
-        privateData: {
-          discoveredServiceVia: null,
-          paypalMerchantId:req.body.resource.payer.payer_id,
-          listingPaidFor:listingDetails
 
-        },
-        metadata: {
-          identityVerified: true
-        }
-       
-      }, {
-        expand: true,
-        include: ["profileImage"]
-      }).then(res => {
-        console.log(`Success: ${res.status} ${res.statusText}`);
-        
-      })
-      .catch(res=>{
-        console.log(`Request failed with status: ${res.status} ${res.statusText}`);
-      });
+
+  //Get the exiting info for this user before updating
+  integrationSdk.users.show({id: ownerId}).then(res => {
+    const currentListing = res.data.attributes.profile.privateData.listingPaidFor;
+    updateProfileData(currentListing);
+  });
+  
+
+  const updateProfileData = (currentListings)=>{
+    const updatedListing = {currentListings,...listingDetails};
+    integrationSdk.users.updateProfile({
+      id: ownerId,
+
+      privateData: {
+        discoveredServiceVia: null,
+        paypalMerchantId:req.body.resource.payer.payer_id,
+        listingPaidFor:updatedListing
+
+      },
+      metadata: {
+        identityVerified: true
+      }
+     
+    }, {
+      expand: true,
+      include: ["profileImage"]
+    }).then(res => {
+      console.log(`Success: ${res.status} ${res.statusText}`);
+      
+    })
+    .catch(res=>{
+      console.log(`Request failed with status: ${res.status} ${res.statusText}`);
+    });
+  };
+  
+    
 
 
 }
